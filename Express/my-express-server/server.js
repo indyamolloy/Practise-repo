f9e44e5716c48250ee3d79c08435f4d0a293195e
Indya 2 <indyacmolloy@gmail.com>
2022-05-22 23:51:33 +1000

    Routes added

--- Express/my-express-server/server.js
+++ Express/my-express-server/server.js
@@ -5,10 +5,6 @@ app.get("/", (req, res) => {
   res.send("hi");
 });
 
-app.get("/about", (req, res) => {
-  res.send("I`m Indya");
-});
-
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
