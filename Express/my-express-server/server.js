c589fc685f9e2646d5816fabf9857e57543abb13
Indya 2 <indyacmolloy@gmail.com>
2022-05-22 23:58:44 +1000

    send file as res

--- Express/my-express-server/server.js
+++ Express/my-express-server/server.js
@@ -1,8 +1,16 @@
 import express from "express";
+import path from "path";
+
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("hi");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000, () => {
