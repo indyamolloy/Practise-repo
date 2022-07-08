9a039ae3bf2ee7bcebe8574cc5d7d748d5f60318
Indya 2 <indyacmolloy@gmail.com>
2022-07-08 14:47:30 +0100

    typescript types practice

---
+++ 04-TypeScript/sandbox.js
@@ -0,0 +1,7 @@
+var names = ["indya", "kieran", "ellie"];
+names[0] = 12;
+console.log(names);
+var dog = {
+    name: "doggy",
+    cute: true
+};
---
+++ 04-TypeScript/sandbox.ts
@@ -0,0 +1,12 @@
+let names: (string | number)[] = ["indya", "kieran", "ellie"];
+
+names[0] = 12;
+
+console.log(names);
+
+let dog = {
+  name: "doggy",
+  cute: true,
+};
+
+let age: number;
