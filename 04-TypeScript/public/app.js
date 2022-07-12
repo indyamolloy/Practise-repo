fed83782f76c59de84b616cba9d1a0730667f395
Indya 2 <indyacmolloy@gmail.com>
2022-07-12 13:34:26 +0100

    enums and tuples

--- 04-TypeScript/public/app.js
+++ 04-TypeScript/public/app.js
@@ -1,4 +1,8 @@
-"use strict";
+import { Invoice } from "./classes/invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
+import { Payment } from "./classes/Payment.js";
+let docOne;
+let docTwo;
 const anchor = document.querySelector("a");
 console.log(anchor.href);
 const form = document.querySelector("form");
@@ -8,25 +12,47 @@ const type = document.querySelector("#type");
 const tofrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+const ul = document.querySelector("ul");
+const list = new ListTemplate(ul);
 alsoForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === "invoice") {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, "end");
 });
-//classes
-class Invoice {
-    //   client: string;
-    //   details: string;
-    //   amount: number;
-    constructor(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    format() {
-        return `${this.client} owes £${this.amount} for ${this.details}`;
-    }
-}
 const invOne = new Invoice("mario", "work on website", 20);
 const invTwo = new Invoice("Me", "work on car", 40);
 let invoices = [];
 invoices.forEach((inv) => console.log(inv.client, inv.amount, inv.format()));
+const me = {
+    name: "indya",
+    age: 24,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        return amount;
+    },
+};
+const greetPerson = (person) => {
+    console.log(`hi, ${person.name}`);
+};
+greetPerson(me);
+//ENUMS
+var ResourceType;
+(function (ResourceType) {
+    ResourceType[ResourceType["BOOK"] = 0] = "BOOK";
+    ResourceType[ResourceType["AUTHOR"] = 1] = "AUTHOR";
+    ResourceType[ResourceType["FILM"] = 2] = "FILM";
+    ResourceType[ResourceType["DIRECTOR"] = 3] = "DIRECTOR";
+})(ResourceType || (ResourceType = {}));
+const doc = {
+    ResourceType: ResourceType.BOOK,
+};
+//tuples
+let tup = ["dog", 13, true];
--- 04-TypeScript/src/app.ts
+++ 04-TypeScript/src/app.ts
@@ -1,3 +1,12 @@
+import { Invoice } from "./classes/invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
+import { HasFormatter } from "./interfaces/HasFormatter.js";
+import { Payment } from "./classes/Payment.js";
+
+let docOne: HasFormatter;
+
+let docTwo: HasFormatter;
+
 const anchor = document.querySelector("a")!;
 
 console.log(anchor.href);
@@ -12,27 +21,21 @@ const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
+const ul = document.querySelector("ul")!;
+const list = new ListTemplate(ul);
+
 alsoForm.addEventListener("submit", (e: Event) => {
   e.preventDefault();
-  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
-});
 
-//classes
-class Invoice {
-  //   client: string;
-  //   details: string;
-  //   amount: number;
-
-  constructor(
-    readonly client: string,
-    private details: string,
-    public amount: number
-  ) {}
-
-  format() {
-    return `${this.client} owes £${this.amount} for ${this.details}`;
+  let doc: HasFormatter;
+  if (type.value === "invoice") {
+    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+  } else {
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
   }
-}
+
+  list.render(doc, type.value, "end");
+});
 
 const invOne = new Invoice("mario", "work on website", 20);
 const invTwo = new Invoice("Me", "work on car", 40);
@@ -40,3 +43,45 @@ const invTwo = new Invoice("Me", "work on car", 40);
 let invoices: Invoice[] = [];
 
 invoices.forEach((inv) => console.log(inv.client, inv.amount, inv.format()));
+
+//interface
+
+interface IsPerson {
+  name: string;
+  age: number;
+  speak(a: string): void;
+  spend(a: number): number;
+}
+
+const me: IsPerson = {
+  name: "indya",
+  age: 24,
+  speak(text: string): void {
+    console.log(text);
+  },
+  spend(amount: number): number {
+    return amount;
+  },
+};
+
+const greetPerson = (person: IsPerson) => {
+  console.log(`hi, ${person.name}`);
+};
+
+greetPerson(me);
+
+//ENUMS
+enum ResourceType {
+  BOOK,
+  AUTHOR,
+  FILM,
+  DIRECTOR,
+}
+
+const doc = {
+  ResourceType: ResourceType.BOOK,
+};
+
+//tuples
+
+let tup: [string, number, boolean] = ["dog", 13, true];
